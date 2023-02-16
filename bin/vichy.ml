b9d247e71d1d93cd73cb9b9498157696830457e4
momoladebrouill <momoladebrouill@gmail.com>
2023-02-16 11:02:25 +0100

    just got hannoïd :rofl:

--- bin/vichy.ml
+++ bin/vichy.ml
@@ -4,11 +4,13 @@ let h = 500
 let slabWidth = 10
 let slabHeight = 14
 
-type slab = None | Slab of {size:int; next: slab; color: Raylib.Color.t }
+type slab = 
+    Null 
+    | Slab of {size: int; next: slab; color: Raylib.Color.t }
 
 type tower = { 
   top : slab;
-  index: int;
+  index : int;
 }
 
 type gamestate = {
@@ -18,15 +20,21 @@ type gamestate = {
   prevKeys : (bool*bool*bool*bool*bool)
 }
 
+let random_color () =
+  match (Random.int 3) with 
+  | 0 -> Raylib.Color.red 
+  | 1 -> Raylib.Color.blue 
+  | 2 -> Raylib.Color.green 
+  | _ -> failwith "arthur palla : c'est super bizarre si ça bug ici"
+
 let generate_tower n =
-  (* n quantité s taille du plus gros*)
   let rec gen acc i =
     if i = 0 then acc
-    else gen (Slab {size = i; next = acc; color = match (Random.int 3) with | 0 -> Raylib.Color.red | 1 -> Raylib.Color.blue | 2 -> Raylib.Color.green | _ -> failwith "no"}) (i-1)
+    else gen (Slab {size = i; next = acc; color = random_color ()} ) (i-1)
   in
-  gen None n
+  gen Null n
 
-let generate_towers ()=
+let generate_towers () =
   let rec gen acc i =
     if i = 0 then acc
     else gen ({top = generate_tower 3; index = i-1}::acc) (i-1)
@@ -41,18 +49,31 @@ let setup () =
   Raylib.set_target_fps 60;
   { 
     towers = generate_towers ();
-    hold = None;
-    current = 0; prevKeys = (false, false, false, false, false);
+    hold = Null;
+    current = 0; 
+    prevKeys = (false, false, false, false, false);
   }
+
+let draw_slab x y b =
+      match b with
+      Null -> ()
+  | Slab b ->
+      Raylib.draw_rectangle (x - b.size * slabWidth) y (b.size * slabWidth * 2) slabHeight b.color 
 
 let draw_tower top x y =
   let rec aux t x y = 
     match t with
-    | None -> y
-    | Slab b -> let ny = (aux b.next x y) - slabHeight in
-      Raylib.draw_rectangle (x - b.size * slabWidth) ny (b.size * slabWidth * 2) slabHeight b.color; 
-      ny
+    | Null -> y
+    | Slab b -> let ny = (aux b.next x y) - slabHeight in draw_slab x ny (Slab b); ny
+      (* Mon reuf ?? très sus *)
   in let _ = aux top x y in ()
+
+let rec first_not_none l default =
+    match l with
+    [] -> default
+    | t::q -> match t with 
+      |None -> first_not_none q default 
+      |Some s -> s 
 
 
 let rec loop gamestate =
@@ -61,39 +82,48 @@ let rec loop gamestate =
     let open Raylib in
     begin_drawing ();
     clear_background Color.black;
-
+    draw_slab (w/2) (3*h/4) gamestate.hold;
     List.iteri (fun i tow-> 
-      let a, b = towerPos i in
-        draw_rectangle (a-slabWidth + 3) (b-slabHeight*6) ((slabWidth - 3)*2) (slabHeight*6) (if gamestate.current = tow.index then Color.yellow else Color.raywhite);
-        draw_tower tow.top a b; 
-        ()
+        let x, y = towerPos i in
+        draw_rectangle 
+          (x-slabWidth + 3) (y-slabHeight*6) 
+          ((slabWidth - 3)*2) (slabHeight*6) 
+          (if gamestate.current = tow.index then Color.yellow else Color.raywhite);
+        draw_tower tow.top x y; 
       ) gamestate.towers;
 
     end_drawing ();
 
 
-    let _ , b, _, d, e = gamestate.prevKeys in
+    let up , right, down, left, space = gamestate.prevKeys in
 
     let current = 
-    let open Raylib in
-    if is_key_down Key.Left && not d then (((gamestate.current - 1) mod 9) + 9) mod 9
-    else if is_key_down Key.Right && not b then ((gamestate.current + 1) mod 9)
-    else gamestate.current in
- 
-    let towers = List.map (fun tow -> 
-      if is_key_down Key.Space && not e && current = tow.index then 
-        {top = (match tow.top with None -> None | Slab b -> b.next); index = tow.index} 
-      else 
-        tow
-    ) gamestate.towers in
+      (if is_key_down Key.Left && not left then 
+        (((gamestate.current - 1) mod 9) + 9)
+      else if is_key_down Key.Right && not right then 
+        (gamestate.current + 1) 
+      else if is_key_down Key.Down && not down then
+         (gamestate.current +3)
+      else if is_key_down Key.Up && not up then
+         (((gamestate.current - 3)mod 9) +9)
+      else gamestate.current) mod 9
+    in
 
-      
+    let towershold = 
+      List.map (fun tow -> 
+          if is_key_down Key.Space && not space && (current = tow.index) then 
+            match gamestate.hold with
+            | Null -> ({top = (match tow.top with Null -> Null | Slab b -> b.next); index = tow.index},Some tow.top)
+            | Slab b -> ({top = Slab {size = b.size ; next = tow.top ; color = b.color}; index = tow.index},Some Null)
+          else (tow,None)
+        ) gamestate.towers 
+    in
 
     let gamestate' = { 
-      towers = towers;
+      towers = List.map (fun (tow,_) -> tow) towershold;
       current = current;
-      prevKeys = (false, is_key_down Key.Right, false, is_key_down Key.Left, is_key_down Key.Space);
-      hold = None
+      prevKeys = (is_key_down Key.Up, is_key_down Key.Right, is_key_down Key.Down, is_key_down Key.Left, is_key_down Key.Space);
+      hold = first_not_none (List.map (fun (_,hold) -> hold) towershold) gamestate.hold;
     } in
     loop gamestate'
 
